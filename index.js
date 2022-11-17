d8fabc2c571dc2b239b8751754ff535130f5a45f
LydiaZ07 <zhulydia07@gmail.com>
2022-11-17 13:14:43 -0500

    day1

---
+++ index.js
@@ -0,0 +1,7 @@
+const DOMSelectors = {
+    button:document.getElementById("btn"),
+    text:document.querySelector("text"),
+    point:document.querySelectorAll(".point"),
+    box:document.getElementById("big-black-box")
+};
+console.log(DOMSelectors.box);
