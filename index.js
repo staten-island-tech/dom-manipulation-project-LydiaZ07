c615534e11599015a15e6528db6c5092380cbf1f
LydiaZ07 <zhulydia07@gmail.com>
2022-11-21 13:15:12 -0500

    progress

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const DOMSelectors = {
     input1:document.querySelector("#texture"),
     input2:document.querySelector("type"),
     input3:document.getElementById("real/fake"),
-    output:document.getElementById("out")
+    output:document.getElementById("out"),
+    display:document.getElementById("display")
 };
- 
-DOMSelectors.button.addEventListener("click", function (){
-    let input1 = DOMSelectors.input1.value;
-    console.log(input1);
+btn.addEventListener('click', run);
+function run(e){
+    console.log('helo');
+e.preventDefault();
+  display.insertAdjacentHTML(`afterend`, display.innerHTML);
+};
+display.innerHTML = `<div class="display-card">
+<img class="display-img" src="cookie pic.jpg">
+<h2 class="display-artist">Beatles</h2>
+<h3 class="display-album">Abbey Road</h3>
+ <button class="remove btn">Remove Album</button>`
 
-});
