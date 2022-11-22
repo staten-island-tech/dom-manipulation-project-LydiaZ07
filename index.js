eaea3ab1ff54c32c8c1dfe063c12f5539ea02ed7
LydiaZ07 <zhulydia07@gmail.com>
2022-11-22 03:19:04 -0500

    next
    next

--- index.js
+++ index.js
@@ -3,20 +3,67 @@ const DOMSelectors = {
     text:document.querySelector("text"),
     box:document.getElementById("big-black-box"),
     input1:document.querySelector("#texture"),
-    input2:document.querySelector("type"),
-    input3:document.getElementById("real/fake"),
+    input2:document.querySelector("#type"),
+    input3:document.getElementById("extra"),
     output:document.getElementById("out"),
     display:document.getElementById("display")
 };
-btn.addEventListener('click', run);
-function run(e){
-    console.log('helo');
-e.preventDefault();
-  display.insertAdjacentHTML(`afterend`, display.innerHTML);
-};
-display.innerHTML = `<div class="display-card">
-<img class="display-img" src="cookie pic.jpg">
-<h2 class="display-artist">Beatles</h2>
-<h3 class="display-album">Abbey Road</h3>
- <button class="remove btn">Remove Album</button>`
+
+DOMSelectors.button.addEventListener("click", function (e) {
+e.preventDefault(); 
+  let input1 = DOMSelectors.input1.value;
+  let input2 = DOMSelectors.input2.value;
+  let input3 = DOMSelectors.input3.value;
+  console.log(input1, input2, input3);
+  result();
+  
+  function result(){
+    DOMSelectors.button.insertAdjacentHTML("afterbegin", '<p> You have created a ${input1} ${input2} ${input3} cookie, enjoy!</p>');
+  DOMSelectors.input1.value = "";
+  DOMSelectors.input2.value = "";
+  DOMSelectors.input3.value = "";
+  }
+
+  // DOMSelectors.display .insertAdjacentHTML("afterbegin", '<p> You have created a ${input1} ${input2} ${input3} cookie, enjoy!</p>');
+  // DOMSelectors.input1.value = "";
+  // DOMSelectors.input2.value = "";
+  // DOMSelectors.input3.value = "";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// btn.addEventListener('click', run);
+// function run(){
+//   display.insertAdjacentHTML(`afterend`, display.innerHTML);
+  // display.innerHTML = `<div class="display-card">`
+// };
+// <img class="display-img" src="cookie pic.jpg">
+// </img>
 
