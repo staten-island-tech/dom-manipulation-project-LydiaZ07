78b85209fc7312d3288daae9e358d5ed867fb688
LydiaZ07 <zhulydia07@gmail.com>
2022-11-28 07:52:33 -0500

    l

--- index.js
+++ index.js
@@ -2,33 +2,40 @@ const DOMSelectors = {
     button:document.getElementById("btn"),
     text:document.querySelector("text"),
     box:document.getElementById("big-black-box"),
-    input1:document.querySelector("#texture"),
-    input2:document.querySelector("#type"),
-    input3:document.getElementById("extra"),
+    input1:document.getElementById("input1"),
+    input2:document.getElementById("input2"),
+    input3:document.getElementById("input3"),
     output:document.getElementById("out"),
     display:document.getElementById("display")
 };
 
 DOMSelectors.button.addEventListener("click", function (e) {
 e.preventDefault(); 
-  let input1 = DOMSelectors.input1.value;
-  let input2 = DOMSelectors.input2.value;
-  let input3 = DOMSelectors.input3.value;
-  console.log(input1, input2, input3);
+let input1 = DOMSelectors.input1.value;
+let input2 = DOMSelectors.input2.value;
+let input3 = DOMSelectors.input3.value;
   result();
-  
+});
   function result(){
-    DOMSelectors.button.insertAdjacentHTML("afterbegin", '<p> You have created a ${input1} ${input2} ${input3} cookie, enjoy!</p>');
+    DOMSelectors.display.insertAdjacentHTML("afterbegin", '<p> You have created a ${input1} ${input2} ${input3} cookie, enjoy!</p>');
+
   DOMSelectors.input1.value = "";
   DOMSelectors.input2.value = "";
   DOMSelectors.input3.value = "";
-  }
+  };
+  function clear(){
+    let clear = document.querySelectorAll(".remove");
+    clear.forEach((el) => {
+      el.addEventListener("click", function (el) {
+        this.parentElement.remove();
 
+  }
+})}
   // DOMSelectors.display .insertAdjacentHTML("afterbegin", '<p> You have created a ${input1} ${input2} ${input3} cookie, enjoy!</p>');
   // DOMSelectors.input1.value = "";
   // DOMSelectors.input2.value = "";
   // DOMSelectors.input3.value = "";
-});
+
 
 
 
