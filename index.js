d13f2cfbce05b6bbfbd2a1188cdced080e395020
LydiaZ07 <zhulydia07@gmail.com>
2022-12-02 13:14:53 -0500

    n

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const DOMSelectors = {
     input2:document.getElementById("input2"),
     input3:document.getElementById("input3"),
     display:document.getElementById("display"),
-    form: document.getElementById("form"),
+    form: document.getElementById("form")
 };
 
 function result(perfectCookie){
-  DOMSelectors.display.insertAdjacentHTML(`beforeend`, `<p> You have created a ${perfectCookie.input1}, ${perfectCookie.input2}, ${perfectCookie.input3} cookie enjoy!</p> 
-  <div><button id="remove">Remove album </button>
+  DOMSelectors.display.insertAdjacentHTML(`beforeend`, `<div id=ptext><p> You have created a ${perfectCookie.input1}, ${perfectCookie.input2}, ${perfectCookie.input3} cookie enjoy!</p> </div>
+  <div><button id="bye">Remove album </button>
   </div>`);
   console.log(perfectCookie.input1)
 DOMSelectors.input1.value = "";
@@ -25,12 +25,11 @@ perfectCookie.input1= DOMSelectors.input1.value;
 perfectCookie.input2 = DOMSelectors.input2.value;
 perfectCookie.input3 = DOMSelectors.input3.value;
   result(perfectCookie);
-
-  
-const bye = document.querySelectorAll("#remove");
-bye.forEach((button) =>
-button.addEventListener("click", function(input1, input2, input3){
-result.target.remove(display);
-}))
 });
 
+const bye = getElementById('bye');
+bai.addEventListener(`click`, function clicky(event){
+  console.log(event.target);
+  event.target.remove();
+
+})
