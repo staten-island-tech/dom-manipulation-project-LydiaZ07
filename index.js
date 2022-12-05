1d43965e09845460c210bda12f0cd6f0339fe77d
LydiaZ07 <zhulydia07@gmail.com>
2022-12-05 13:15:12 -0500

    l

--- index.js
+++ index.js
@@ -10,28 +10,27 @@ const DOMSelectors = {
 function result(perfectCookie){
   DOMSelectors.display.insertAdjacentHTML(`beforeend`, `<div id=parent>
   <div id=ptext><p> You have created a ${perfectCookie.input1}, ${perfectCookie.input2}, ${perfectCookie.input3} cookie enjoy!</p> </div>
-  <div><button id="now">Remove Cookie </button>
+  <div><button class="now">Remove Cookie </button>
   </div></div>`);
 DOMSelectors.input1.value = "";
 DOMSelectors.input2.value = "";
 DOMSelectors.input3.value = "";
-
-const bye = document.getElementById("bye");
-console.log(bye)
 };
 
 DOMSelectors.form.addEventListener("submit", function (e) {
 e.preventDefault(); 
-let perfectCookie={};
+const perfectCookie={};
 perfectCookie.input1= DOMSelectors.input1.value;
 perfectCookie.input2 = DOMSelectors.input2.value;
 perfectCookie.input3 = DOMSelectors.input3.value;
   result(perfectCookie);
-  hi();
 });
 
-const button = document.queSelectorAll("#now");
-// bye.forEach((button)=> 
-button.addEventListener("click", function (){
+const buttons = document.querySelectorAll("now");
+console.log(buttons);
+buttons.forEach((button)=> {
+ button.addEventListener("click", removeButton)});
+function removeButton() {
  /*  const cookie = this.parentElement.parentElement; */
-   button.parentElement.remove();});
+ console.log("hello");
+   button.parentElement.parentElement.remove()};
